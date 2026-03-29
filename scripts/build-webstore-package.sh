2f768d7a5f74576430a891884d39f7deca9864c8
Fuat Altintas <faltintas24@gmail.com>
2026-03-29 16:24:49 +0200

    Add versioned webstore release package

--- scripts/build-webstore-package.sh
+++ scripts/build-webstore-package.sh
@@ -6,6 +6,8 @@ ROOT_DIR=$(CDPATH= cd -- "$(dirname "$0")/.." && pwd)
 DIST_DIR="$ROOT_DIR/dist"
 STAGE_DIR="$DIST_DIR/wikarte-webstore"
 ZIP_PATH="$DIST_DIR/wikarte-webstore.zip"
+VERSION=$(node -p "require('$ROOT_DIR/package.json').version")
+VERSIONED_ZIP_PATH="$DIST_DIR/wikarte-webstore-v$VERSION.zip"
 
 INCLUDE_PATHS="
 manifest.json
@@ -36,7 +38,9 @@ done
 
 find "$STAGE_DIR" -name '.DS_Store' -delete
 
-rm -f "$ZIP_PATH"
+rm -f "$ZIP_PATH" "$VERSIONED_ZIP_PATH"
 cd "$STAGE_DIR"
 zip -qr "$ZIP_PATH" .
+cp "$ZIP_PATH" "$VERSIONED_ZIP_PATH"
 printf '%s\n' "$ZIP_PATH"
+printf '%s\n' "$VERSIONED_ZIP_PATH"
